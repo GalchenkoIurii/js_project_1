0300ff656421a2ce81a8cdfc4113f107e9362b90
GalchenkoIurii <galchenkou@gmail.com>
2019-08-28 19:56:50 +0300

    adding script

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let str = 'some string';
+let money = +prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-function myFunc() {
-    let a = 12;
-    for (let i = 0; i < 10; i++) {
-        console.log(i);
-    }
-    for (let j = 0; j < 10; j++) {
-        console.log(j);
-    }
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+for (let i = 0; ){
+
 }
-myFunc();
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
